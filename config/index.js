4a981e1026fe4f0aba0f89d2fa11474037c8a761
ShawnCZQ <113523414+ShawnCZQ@users.noreply.github.com>
2022-09-15 00:10:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx7c3c0a7f85268e6a",
+  APP_ID:"wx7c3c0a7f85268e6a",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "4d56d79f4fca81004291df949af02db1",
+  APP_SECRET:"ef7625a51bcaf14daaace9662c45567f",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -23,11 +23,11 @@ export const config = {
   USERS:[
     {
       // 想要发送的人的名字
-      name: "致远",
+      name:"致远",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id:"obhrX5i98bOTKKD1k5wvk84j7Vm8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId:"obhrX5i98bOTKKD1k5wvk84j7Vm8",
+      useTemplateId:"OVb7ME3KlvzAH3LWvbHXiDjoaHLPzw_MmjauvIt1Kms",
       // 所在省份
       province: "江苏",
       // 所在城市
